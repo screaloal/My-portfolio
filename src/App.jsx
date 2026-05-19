247323bf8bfc3dc1fa23197de5671cebf58ebf9c
screaloal <belloayomipo575@gmail.com>
2026-05-19 13:12:34 +0100

    fix: revert analytics import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './index.css'
-import { Analytics } from '@vercel/analytics'
+import { Analytics } from '@vercel/analytics/react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
